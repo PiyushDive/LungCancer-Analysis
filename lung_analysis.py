9852eceb0917a23ed9149172943ad36bf0f997d9
PiyushDive <piyushdive835@gmail.com>
2025-09-20 18:38:35 +0530

    Final Commit

--- lung_analysis.py
+++ lung_analysis.py
@@ -1,13 +1,19 @@
 import pandas as pd
 
-data = pd.read_csv("lung_cancer_dataset.csv")
-
-data[["copd_diagnosis", "family_history", "lung_cancer"]] = data[["copd_diagnosis", "family_history",
+uncleaned = pd.read_csv("lung_cancer_dataset.csv")
+uncleaned[["copd_diagnosis", "family_history", "lung_cancer"]] = uncleaned[["copd_diagnosis", "family_history",
 "lung_cancer"]].replace({"Yes":1, "No":0})
+
+uncleaned.to_csv("cleaned_lung_cancer.csv", index=False)
+data = pd.read_csv("cleaned_lung_cancer.csv")
 
 # it inherits data Dataframe and has all the columns.
 cancer = data[data["lung_cancer"] == 1]
 nocancer = data[data["lung_cancer"] == 0]
+
+avg_age_cancer = cancer["age"].mean()
+avg_age_nocancer = nocancer["age"].mean()
+
 avg_male_cancer = (cancer["gender"] == "Male").mean()*100
 avg_female_cancer = (cancer["gender"] == "Female").mean()*100
 avg_male_nocancer = (nocancer["gender"] == "Male").mean()*100
@@ -16,12 +22,6 @@ avg_female_nocancer = (nocancer["gender"] == "Female").mean()*100
 # family history
 familyHistoryCancer = (cancer['family_history'] == 1).mean()*100
 familyHistoryNocancer = (nocancer['family_history'] == 1).mean()*100
-
-
-avg_age_cancer = cancer["age"].mean()
-avg_age_nocancer = nocancer["age"].mean()
-
-
 
 print(data.info())
 print(data.describe())
